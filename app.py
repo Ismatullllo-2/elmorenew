079343ea0c7fadb9983578296c5ad77776ffa318
Ismatullllo-2 <Ismatullon.orig@gmail.com>
2025-06-23 07:48:49 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+#-*- coding: utf-8 -*-
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Dummy user data for demonstration purposes
+users = {}
+posts = []
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_id = len(users) + 1  # Simple ID assignment
+        new_user = User(user_id, username, password)
+        users[user_id] = new_user
+        flash('da', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = next((u for u in users.values() if u.username == username and u.password == password), None)
+        if user:
+            login_user(user)
+            return redirect(url_for('forum'))
+        else:
+            flash('not', 'danger')
+    return render_template('login.html')
+@app.route('/forum', methods=['GET', 'POST'])
+@login_required
+def forum():
+    return render_template('forum.html', posts=posts)
+
+@app.route('/create_post', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = {'title': title, 'content': content, 'author': current_user.username}
+        posts.append(new_post)
+        flash('hm!', 'success')
+        return redirect(url_for('forum'))
+    return render_template('create_post.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+	return 'hello, {current_user.username}! <a href="/logout">quit</a>'
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
